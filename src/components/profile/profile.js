8da0320d7cfb8eb409c9be8efd2405f9cd1eed72
haritejakoduri <haritejakoduri1234@gmail.com>
2021-01-22 22:50:16 +0530

    all issues solved for now

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -5,10 +5,10 @@ function Profile(){
    return(
       <Container className='card'>
          <Row>
-            <Col lg={6} md={6} xl={6} sm={6} xs={6}>
+            <Col lg={6} md={5} xl={5} sm={5} xs={5}>
                <Image src="https://haritejakoduri.github.io/resume/photo.jpg" className="card-img" style={{height:"100px"}} />
             </Col>
-            <Col lg={6} md={6} xl={6} sm={6} xs={6}>
+            <Col lg={7} md={7} xl={7} sm={7} xs={7}>
                <h3 className="initial">Koduri</h3>
                <h2 className="name">HariTeja</h2>
                <h4 className="prof">JavaScript developer</h4>
